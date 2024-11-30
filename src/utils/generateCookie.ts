82e4690371fe43fcdca31b867bf22251e1e9c8ab
jeffrey <jeffreyigiemeh16@gmail.com>
2024-11-30 01:33:08 -0800

    new commit

--- src/utils/generateCookie.ts
+++ src/utils/generateCookie.ts
@@ -2,10 +2,11 @@ import { Response } from "express";
 
 
 export const GenerateCookie = (token:string,res:Response)=>{
+   const isSecure = process.env.SECURE === 'true';
    return res.cookie("token",token,{
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly:true,
-    sameSite: "none", // or "lax"
-    secure: true,   
+    sameSite:"none",
+    secure:isSecure
    })
 }
